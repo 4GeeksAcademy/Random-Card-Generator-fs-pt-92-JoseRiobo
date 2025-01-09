652bb5b5f29e00d5f99c5fa42d95e7b1d1a43d01
joseriobo <joseriobo7@gmail.com>
2025-01-09 19:24:59 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,62 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let number = ["1", "2", "3", "4", "5", "6", "7", "J", "Q", "K"];
+let symbol = ["♣", "♠", "♥", "♦"];
+
+window.onload = function randomCard() {
+  const randomNumber = Math.floor(Math.random() * number.length);
+  const cardNumber = number[randomNumber];
+
+  let aux = document.querySelector(".theMiddle");
+  aux.textContent = cardNumber;
+
+  const randomSymbol = Math.floor(Math.random() * symbol.length);
+  const cardSymbolBottom = symbol[randomSymbol];
+  let auxSymbolBottom = document.querySelector(".bottomRight");
+  auxSymbolBottom.textContent = cardSymbolBottom;
+
+  const cardSymbolTop = symbol[randomSymbol];
+  let auxSymbolTop = document.querySelector(".topLeft");
+  auxSymbolTop.textContent = cardSymbolTop;
+
+  cardstyleTop(symbol[randomSymbol]);
+  cardstyleBottom(symbol[randomSymbol]);
 };
+
+function cardstyleTop(randomSymbolTop) {
+  let card = document.querySelector(".topLeft");
+  card.className = "";
+
+  if (randomSymbolTop === "♣") {
+    card.classList.add("spade");
+  }
+  if (randomSymbolTop === "♠") {
+    card.classList.add("club");
+  }
+  if (randomSymbolTop === "♥") {
+    card.classList.add("heart");
+  } else if (randomSymbolTop === "♦") {
+    card.classList.add("diamond");
+  }
+}
+
+function cardstyleBottom(randomSymbolBottom) {
+  let card = document.querySelector(".bottomRight");
+  card.className = "";
+
+  if (randomSymbolBottom === "♣") {
+    card.classList.add("spadeBottom");
+  }
+  if (randomSymbolBottom === "♠") {
+    card.classList.add("clubBottom");
+  }
+  if (randomSymbolBottom === "♥") {
+    card.classList.add("heartBottom");
+  } else if (randomSymbolBottom === "♦") {
+    card.classList.add("diamondBottom");
+  }
+}
